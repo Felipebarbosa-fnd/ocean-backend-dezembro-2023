337914af083f4ff11e67933ef5d7be72870ca9ce
Felipebarbosa-fnd <152872888+Felipebarbosa-fnd@users.noreply.github.com>
2023-12-05 14:42:44 +0000

    Configurando dotenv

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const { MongoClient, ObjectId } = require('mongodb')
 
-const dbUrl = "mongodb+srv://admin:4knfHCWK2scUENTB@cluster0.swydtn3.mongodb.net"
+const dbUrl = process.env.DATABASE_URL
 const client = new MongoClient(dbUrl)
-const dbName = "ocean-backend-dezembro-2023"
+const dbName = process.env.DATABASE_NAME
 
 async function main() {
   console.log("Conectando ao banco de dados...")
